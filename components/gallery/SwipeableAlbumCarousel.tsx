1d3bb88aeb478a1ea7f9b301c84ead88569bfa36
Tek-Planet <oluwabishefiu@gmail.com>
2025-12-04 15:32:24 +0000

    worked on the photo list of the gallery screen

---
+++ components/gallery/SwipeableAlbumCarousel.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { View, StyleSheet, FlatList } from 'react-native';
+import AlbumHeroCard from './AlbumHeroCard';
+import { Album } from '../../data/gallery';
+
+interface SwipeableAlbumCarouselProps {
+  albums: Album[];
+  onAlbumChange: (album: Album) => void;
+}
+
+const SwipeableAlbumCarousel: React.FC<SwipeableAlbumCarouselProps> = ({ albums, onAlbumChange }) => {
+  const onViewableItemsChanged = ({ viewableItems }) => {
+    if (viewableItems.length > 0) {
+      onAlbumChange(viewableItems[0].item);
+    }
+  };
+
+  const viewabilityConfig = { viewAreaCoveragePercentThreshold: 50 };
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={albums}
+        renderItem={({ item }) => <AlbumHeroCard album={item} />}
+        keyExtractor={item => item.id}
+        horizontal
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        onViewableItemsChanged={onViewableItemsChanged}
+        viewabilityConfig={viewabilityConfig}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    height: 250,
+  },
+});
+
+export default SwipeableAlbumCarousel;
---
+++ screens/GalleryScreen.tsx
@@ -0,0 +1,44 @@
+import React, { useState, useMemo } from 'react';
+import { View, StyleSheet, TouchableOpacity } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import SwipeableAlbumCarousel from '../components/gallery/SwipeableAlbumCarousel';
+import AlbumPhotosSection from '../components/gallery/AlbumPhotosSection';
+import { albums as allAlbums, Album } from '../data/gallery';
+
+const GalleryScreen: React.FC = ({ route }) => {
+  const navigation = useNavigation();
+  const { albumId } = route.params;
+
+  const initialAlbum = useMemo(() => allAlbums.find(album => album.id === albumId), [albumId]);
+  const [currentAlbum, setCurrentAlbum] = useState<Album>(initialAlbum);
+
+  const handleAlbumChange = (album: Album) => {
+    setCurrentAlbum(album);
+  };
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.backButton} onPress={() => navigation.goBack()}>
+        <Ionicons name="arrow-back" size={24} color="white" />
+      </TouchableOpacity>
+      <SwipeableAlbumCarousel albums={allAlbums} onAlbumChange={handleAlbumChange} />
+      <AlbumPhotosSection photos={currentAlbum.photos} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+  },
+  backButton: {
+    position: 'absolute',
+    top: 40,
+    left: 20,
+    zIndex: 1,
+  },
+});
+
+export default GalleryScreen;
