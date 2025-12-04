fe2b4a35dabb79d4e2b6f804ed3137f61a5f4783
Tek-Planet <oluwabishefiu@gmail.com>
2025-12-04 17:45:43 +0000

    work on animation for gallery

--- screens/PhotoViewerScreen.tsx
+++ screens/PhotoViewerScreen.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { View, Image, StyleSheet, TouchableOpacity } from 'react-native';
 import { RouteProp, useNavigation } from '@react-navigation/native';
 import { RootStackParamList } from '../types';
-import { Ionicons } from '@expo/vector-icons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 type PhotoViewerScreenRouteProp = RouteProp<RootStackParamList, 'PhotoViewer'>;
 
