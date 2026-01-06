71d7b4a9d79aaf4c0ddefdaaa7fbd088d5975d21
Tek-Planet <oluwabishefiu@gmail.com>
2026-01-06 16:35:28 +0000

    worked on the photo recognition function

--- grid/src/index.ts
+++ grid/src/index.ts
@@ -5,16 +5,19 @@ import { admin, db } from "./lib/firebase";
 
 // Import the new matching function
 import { processSurveyAndFindMatches } from "./matching";
+import { onPhotoCreated, indexUserProfilePicture } from "./face-recognition";
 
 const stripe = new Stripe(functions.config().stripe.secret_key, {
   apiVersion: "2023-10-16",
 } as any);
 
 //================================================================================
-// EXPORT NEW MATCHING FUNCTION
+// EXPORT FUNCTIONS
 //================================================================================
 
 export { processSurveyAndFindMatches };
+export { onPhotoCreated };
+export { indexUserProfilePicture };
 
 type SelectedTiers = Record<string, number>;
 
